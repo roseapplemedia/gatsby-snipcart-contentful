fa7a0d59321c48828319a68d2e19926767eecfc9
RoseAppleMedia <michael@roseapplemedia.com>
2019-10-06 15:56:00 -0400

    New title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
-const dotenv = require('dotenv')
+const dotenv = require("dotenv");
 
-if (process.env.NODE_ENV !== 'production') {
-  dotenv.config()
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
 }
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Garb`,
+    title: `A Little Cattywampus`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `Reed Barger`,
-    createdAt: 2019,
+    author: `Rose Apple Media`,
+    createdAt: 2019
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -17,25 +17,25 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-images',
-          },
-        ],
-      },
+            resolve: "gatsby-remark-images"
+          }
+        ]
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts`,
-      },
+        path: `${__dirname}/src/posts`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -48,25 +48,25 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+      }
     },
     {
-      resolve: 'gatsby-source-contentful',
+      resolve: "gatsby-source-contentful",
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+      }
     },
     {
-      resolve: 'gatsby-plugin-snipcart',
+      resolve: "gatsby-plugin-snipcart",
       options: {
         apiKey: process.env.SNIPCART_API_KEY,
-        autopop: true,
-      },
-    },
+        autopop: true
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
-}
+  ]
+};
